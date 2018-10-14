a1d5171d700ef3ba893039bc7efad84e84a42dca
Unknown <luis.alvela@gmail.com>
2018-10-14 09:58:41 +0100

    Initial state of the board

---
+++ boardLists.pl
@@ -0,0 +1,12 @@
+initialBoard([
+               [emptyCell, emptyCell, emptyCell, emptyCell, cityPiece, emptyCell, emptyCell, emptyCell, emptyCell, emptyCell],
+               [redSoldier, redSoldier, redSoldier, redSoldier, redSoldier, redSoldier,redSoldierl, redSoldier, redSoldier, redSoldier],
+               [redSoldier, redSoldier, redSoldier, redSoldier, redSoldier, redSoldier,redSoldierl, redSoldier, redSoldier, redSoldier],
+               [redSoldier, redSoldier, redSoldier, redSoldier, redSoldier, redSoldier,redSoldierl, redSoldier, redSoldier, redSoldier],
+               [emptyCell, emptyCell, emptyCell, emptyCell, emptyCell, emptyCell, emptyCell, emptyCell, emptyCell, emptyCell],
+               [emptyCell, emptyCell, emptyCell, emptyCell, emptyCell, emptyCell, emptyCell, emptyCell, emptyCell, emptyCell],
+               [blackSoldier, blackSoldier, blackSoldier, blackSoldier, blackSoldier, blackSoldier, blackSoldier, blackSoldier, blackSoldier, blackSoldier],
+               [blackSoldier, blackSoldier, blackSoldier, blackSoldier, blackSoldier, blackSoldier, blackSoldier, blackSoldier, blackSoldier, blackSoldier],
+               [blackSoldier, blackSoldier, blackSoldier, blackSoldier, blackSoldier, blackSoldier, blackSoldier, blackSoldier, blackSoldier, blackSoldier],
+               [emptyCell, emptyCell, emptyCell, emptyCell, cityPiece, emptyCell,emptyCell, emptyCell,  emptyCell, emptyCell]
+           ]).
