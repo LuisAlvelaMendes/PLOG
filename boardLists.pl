632c3508c8a5ee7853b41ac97071046b6216971c
migudmigu <33071622+migudmigu@users.noreply.github.com>
2018-10-14 16:11:05 +0100

    board intermedio ta

--- boardLists.pl
+++ boardLists.pl
@@ -13,14 +13,14 @@ initialBoard([
 
 intermediumBoard([
                      [emptyCell, emptyCell, emptyCell, emptyCell, cityPiece, emptyCell, emptyCell, emptyCell, emptyCell, emptyCell],
-                     [redSoldier, redSoldier, redSoldier, redSoldier, redSoldier, redSoldier, redSoldier, redSoldier, redSoldier, redSoldier],
-                     [redSoldier, redSoldier, redSoldier, redSoldier, redSoldier, redSoldier, redSoldier, redSoldier, redSoldier, redSoldier],
-                     [redSoldier, redSoldier, redSoldier, redSoldier, redSoldier, emptyCell , redSoldier, redSoldier, redSoldier, redSoldier],
+                     [redSoldier, redSoldier, emptyCell, redSoldier, emptyCell, emptyCell, emptyCell, emptyCell, emptyCell, emptyCell],
+                     [emptyCell, redSoldier, emptyCell, emptyCell, redSoldier, redSoldier, emptyCell, emptyCell, emptyCell, emptyCell],
+                     [emptyCell, emptyCell, redSoldier, emptyCell, emptyCell, emptyCell , emptyCell, redSoldier, emptyCell, redSoldier],
                      [emptyCell, emptyCell, emptyCell, emptyCell, emptyCell, redSoldier, emptyCell, emptyCell, emptyCell, emptyCell],
-                     [emptyCell, emptyCell, emptyCell, emptyCell, emptyCell, emptyCell, emptyCell, emptyCell, emptyCell, emptyCell],
-                     [blackSoldier, blackSoldier, blackSoldier, blackSoldier, blackSoldier, blackSoldier, blackSoldier, blackSoldier, blackSoldier, blackSoldier],
-                     [blackSoldier, blackSoldier, blackSoldier, blackSoldier, blackSoldier, blackSoldier, blackSoldier, blackSoldier, blackSoldier, blackSoldier],
-                     [blackSoldier, blackSoldier, blackSoldier, blackSoldier, blackSoldier, blackSoldier, blackSoldier, blackSoldier, blackSoldier, blackSoldier],
+                     [emptyCell, emptyCell, emptyCell, blackSoldier, blackSoldier, emptyCell, emptyCell, redSoldier, emptyCell, emptyCell],
+                     [emptyCell, blackSoldier, blackSoldier, emptyCell, emptyCell, blackSoldier, emptyCell, redSoldier, emptyCell, emptyCell],
+                     [emptyCell, emptyCell, blackSoldier, emptyCell, emptyCell, emptyCell, emptyCell, emptyCell, emptyCell, emptyCell],
+                     [emptyCell, emptyCell, emptyCell, emptyCell, emptyCell, emptyCell, emptyCell, emptyCell, blackSoldier, emptyCell],
                      [emptyCell, emptyCell, emptyCell, emptyCell, emptyCell, emptyCell, emptyCell, emptyCell,  cityPiece, emptyCell]
            ]).
 
