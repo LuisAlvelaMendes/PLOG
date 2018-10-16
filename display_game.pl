cd3a51d944b154d452808d4df97e4a8f12f86d19
migudmigu <33071622+migudmigu@users.noreply.github.com>
2018-10-16 11:27:57 +0100

    added row IDs

--- display_game.pl
+++ display_game.pl
@@ -1,12 +1,10 @@
 :-include('boardLists.pl').
 
-/* rowID(['1', '2', '3', '4', '5', '6', '7', '8', '9', '10']). TODO: ADD THESE AND A, B, C, D, E .. */
-
 /*Separador de cima/baixo/meio do taubleiro*/
-printGenericSeparator:-write(' _______________________________________'), nl.
+printGenericSeparator:-write('   _______________________________________'), nl.
                        
 /*Spearador do meio */
-printMiddleSeparator:-write('|___|___|___|___|___|___|___|___|___|___|'), nl.
+printMiddleSeparator:-write('  |___|___|___|___|___|___|___|___|___|___|'), nl.
 
 /*TODO: Espaco que fica entre o | e o _ no tabuleiro*/
 
@@ -25,10 +23,13 @@ display_board_line([]):- write('| ') , nl, printMiddleSeparator.
 /*Para cada linha, ou seja, sublista, vai tentando encontrar uma correspondencia entre o elemento a analisar nesta etapa, que vai sendo a cabeca, e o charater a imprimir. Depois usa-se tail rescursion para ir passando ao proximo elemento*/
 display_board_line([Head|Tail]):- getSymbol(Head,Char), printCell(Char), display_board_line(Tail).
 
+displayID(ID):- ID \= '10', write(ID), write(' ').
+displayID(ID):- ID = '10', write(ID).
+
 /*Vai consumindo cada sublista da lista grande, cada linha [emptyCell, emptyCell, ...] neste caso e um "Head", vai-se usando tail recursion para chegar as outras linhas*/
-display_game_aux([]):-nl.
-display_game_aux([Head|Tail]):- display_board_line(Head), display_game_aux(Tail).
+display_game_aux([], []):-nl.
+display_game_aux([Head|Tail], [RHead|RTail]):- displayID(RHead), display_board_line(Head), display_game_aux(Tail, RTail).
 
-display_game(Board):- printGenericSeparator, display_game_aux(Board).
+display_game(Board):- printGenericSeparator, display_game_aux(Board, ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10']).
 
-/*display_game(+Board,+Player) .. para que serve player? e o +?*/
+/*display_game(+Board,+Player)*/
