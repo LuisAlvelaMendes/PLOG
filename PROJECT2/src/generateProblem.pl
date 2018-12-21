12617886d6a3b39e01e3991303b7d918fdfc7c61
Luís Mendes <luis.alvela@gmail.com>
2018-12-21 12:28:09 +0000

    fixed up code a little, tried something but didn't work
    read the .txt file and submit to moodle if you can't find an answer

--- PROJECT2/src/generateProblem.pl
+++ PROJECT2/src/generateProblem.pl
@@ -4,13 +4,13 @@
 :- use_module(library(lists)).
 :- include('utilities.pl').
 
-findCoords([], [], [], _).
-findCoords([X1, X2|Xs], [Y1, Y2|Ys], [L|T], FD):-
+findCoords([], [], _).
+findCoords([X1, X2|Xs], [Y1, Y2|Ys], FD):-
         L in_set FD,
        
         L #= ((X1-X2)*(X1-X2) + (Y1-Y2)*(Y1-Y2)),
                 
-        findCoords(Xs, Ys, T, FD).
+        findCoords(Xs, Ys, FD).
 
 generateRandomHouseCoords(N, HouseCoordsX, HouseCoordsY):-
         initializeRandomSeed,
@@ -18,35 +18,39 @@ generateRandomHouseCoords(N, HouseCoordsX, HouseCoordsY):-
         
         write(N), nl,
         
-        MaxDistance is (N*N + N*N),
+		% Max distance is the diagonal of the square, but coordinates only go from 0 to N-1
+        MaxDistance is ((N-1)*(N-1) + (N-1)*(N-1)),
         
-        %gerar duas distâncias aleatórias e posicionar as casas em relação às distâncias
+        % generate two random distances L1 and L2, they are guaranteed to be distinct.
         gen_2_num(MaxDistance, L1, L2),
         
         write(L1), nl,
-        write(L2),
+        write(L2), nl,
         
-        %obter as coordenadas de cada casa e fornecer isso ao solve
-        
+        % domain variables are our coordinates .. each coordinate only goes from 0 to N-1
         SuperiorLimit is N-1,
+		
+		% generating puzzles with an even number of houses always
         Houses is N*2,
+		% Houses mod 2 #= 0,
         
+		% HouseCoordsX holds all X coordinates, HouseCoords Y holds the Y for each house
         length(HouseCoordsX, Houses),
         length(HouseCoordsY, Houses),
         domain(HouseCoordsX, 0, SuperiorLimit),
         domain(HouseCoordsY, 0, SuperiorLimit),
         
-        %group coords together
+        %group coords together in a format like [[X1, Y1], [X2, Y2] ...] to make sure each point is unique
         makeCoordPairs(HouseCoordsX, HouseCoordsY, Result),
         
         %make sure all coords are different
         dif_all(Result),
         
+		% set of valid distances
         list_to_fdset([L1, L2], FD),
 
-        findCoords(HouseCoordsX, HouseCoordsY, Lengths, FD),
-        
-        nvalue(2, Lengths),
+		% finding coordinates that satisfy that set
+        findCoords(HouseCoordsX, HouseCoordsY, FD),
         
         append(HouseCoordsX, HouseCoordsY, FlatResult),
         
