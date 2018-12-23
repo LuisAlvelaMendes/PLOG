e1b5bba14ebae54fb3bdd680eadb9ed7babebd11
Luis Mendes <luis.alvela@gmail.com>
2018-12-23 09:39:36 +0000

    removed unecessary comments

--- PROJECT2/src/display.pl
+++ PROJECT2/src/display.pl
@@ -1,35 +1,25 @@
 :-include('boardLists.pl').
 
-/*Carateres e a sua correspondencia com elementos na lista interna*/
+
 getSymbol(emptyCell, ' ').
 getSymbol(houseCell, 'H').
 
-/*Imprime um daqueles carateres com barras a volta*/
+
 printCell(Char):- write('| '), write(Char).
 
-/*Imprime o numero da House (supondo que estao seguidos) */
 printHouseNumber(Head,N, NN):- (Head == houseCell, write(N), NN is N+1) ; NN is N. 
 
-/*Caso base: quando ja consumiu toda a linha (sublista da lista grande) so imprime uma barra final e um espaco*/
 display_board_line([],N,N):- write('| ') , nl.
-
-/*Para cada linha, ou seja, sublista, vai tentando encontrar uma correspondencia entre o elemento a analisar nesta etapa, que vai sendo a cabeca, e o charater a imprimir. Depois usa-se tail rescursion para ir passando ao proximo elemento*/
 display_board_line([Head|Tail],N,NNN):- getSymbol(Head,Char), printCell(Char), printHouseNumber(Head,N,NN), display_board_line(Tail,NN,NNN).
-
-/*Vai consumindo cada sublista da lista grande, cada linha [emptyCell, emptyCell, ...] neste caso e um "Head", vai-se usando tail recursion para chegar as outras linhas*/
 display_game_aux([],_):- nl.
 display_game_aux([Head|Tail],N):- display_board_line(Head,N,NN), display_game_aux(Tail,NN).
 
 display_game(Board):- display_game_aux(Board,1).
 
 
-/*Caso base: quando ja consumiu toda a linha (sublista da lista grande) so imprime uma barra final e um espaco*/
 display_board_line2([]):- write('| ') , nl.
 
-/*Para cada linha, ou seja, sublista, vai tentando encontrar uma correspondencia entre o elemento a analisar nesta etapa, que vai sendo a cabeca, e o charater a imprimir. Depois usa-se tail rescursion para ir passando ao proximo elemento*/
 display_board_line2([Head|Tail]):- getSymbol(Head,Char), printCell(Char), display_board_line2(Tail).
-
-/*Vai consumindo cada sublista da lista grande, cada linha [emptyCell, emptyCell, ...] neste caso e um "Head", vai-se usando tail recursion para chegar as outras linhas*/
 display_game_aux2([]):- nl.
 display_game_aux2([Head|Tail]):- display_board_line2(Head), display_game_aux2(Tail).
 
